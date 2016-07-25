fac50eec0ad7689dc6197d3a7d22801b5b2a1784
Hikaru Chang <kensou0012@gmail.com>
2016-07-25 15:51:07 +0800

    js validator 的 email 驗証，空白時改為合法 是否空白交由 required 屬性去驗

--- public/js/conpoz.validator.js
+++ public/js/conpoz.validator.js
@@ -206,7 +206,9 @@ Validator.prototype.valid = function (jqForm) {
 };
 
 Validator.prototype.isRFC822ValidEmail = function (sEmail) {
-
+    if (!sEmail) {
+        return true;
+    }
     var sQtext = '[^\\x0d\\x22\\x5c\\x80-\\xff]';
     var sDtext = '[^\\x0d\\x5b-\\x5d\\x80-\\xff]';
     var sAtom = '[^\\x00-\\x20\\x22\\x28\\x29\\x2c\\x2e\\x3a-\\x3c\\x3e\\x40\\x5b-\\x5d\\x7f-\\xff]+';
