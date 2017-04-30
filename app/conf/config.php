cb32f74ad86e804725fa21258d7617307ecd86d5
Hikaru Chang <kensou0012@gmail.com>
2017-04-30 17:07:56 +0800

    新增讀寫分離功能

--- app/conf/config.php
+++ app/conf/config.php
@@ -5,30 +5,48 @@ $config->route = array(
     'defaultAction' => 'index',
     '404Controller' => 'Error',
     '404Action' => 'http404',
-    );
+);
 $config->autoloadNamespace = array(
     'Conpoz\\App\\Controller\\' => APP_PATH . '/controller/',
     'Conpoz\\App\\Task\\' => APP_PATH . '/task/',
     'Conpoz\\App\\Model\\' => APP_PATH . '/model/',
     'Conpoz\\App\\Lib\\' => APP_PATH . '/lib/',
     'Conpoz\\Core\\Lib\\' => CORE_PATH . '/lib/',
-    );
+);
 $config->db = array(
-    'adapter' => 'mysql',
-    'dbname' => 'hikaru',
-    'host' => '127.0.0.1',
-    'username' => 'root',
-    'password' => 'qeksnopre'
-    );
+    'master' => array(
+        'adapter' => 'mysql',
+        'dbname' => 'hikaru',
+        'host' => '127.0.0.1',
+        'username' => 'root',
+        'password' => 'qeksnopre'
+    ),
+    'slave' => array(
+        array(
+            'adapter' => 'mysql',
+            'dbname' => 'hikaru',
+            'host' => '127.0.0.1',
+            'username' => 'root',
+            'password' => 'qeksnopre'
+        ),
+        array(
+            'adapter' => 'mysql',
+            'dbname' => 'hikaru',
+            'host' => '127.0.0.1',
+            'username' => 'root',
+            'password' => 'qeksnopre'
+        ),
+    )
+);
 $config->mem = array(
     'host' => '127.0.0.1',
     'port' => 11211,
-    );
+);
 $config->ACL = array(
     'publicRole' => 'guest',
     'publicResources' => array(
         array('Index', '*'),
         array('Error', '*'),
-        ),
-    );
+    ),
+);
 return $config;
--- core/lib/Db/DBQuery.php
+++ core/lib/Db/DBQuery.php
@@ -3,65 +3,96 @@
 namespace Conpoz\Core\Lib\Db;
 class DBQuery 
 {
+    const AUTO_RESOURCE_ID = -1;
+    const MASTER_RESOURCE_ID = 0;
+    const SLAVE_RESOURCE_ID = 1;
     static public $bindType = array(
         'boolean' => \PDO::PARAM_BOOL,
         'integer' => \PDO::PARAM_INT,
         'others' => \PDO::PARAM_STR,
         );
-    public $db = null;
+    public $db = array();
     public $sth = null;
-    public $success = false;
+    public $success = array(false, false);
     public $deadlockRetryTimes = 3;
     public $deadlockUsleepTime = 300000; //0.3s
+    public $focusMaster = false;
     private $errorInfo = null;
-    private $dsn = null;
-    private $username = null;
-    private $password = null;
+    private $dsnSet = array();
+    private $username = array();
+    private $password = array();
     private $sqlErrorHandler = null;
     protected $table;
     protected $data;
 
-    public function __construct($params) 
+    public function __construct($dsnSet) 
     {
-        set_error_handler(function($errno, $errstr, $errfile, $errline, array $errcontext) {
-            if (strpos($errstr, 'PDO::__construct(): MySQL server has gone away') !== false || strpos($errstr, "Error while sending QUERY packet") !== false) {
-                throw new \Conpoz\Core\Lib\Db\DBQuery\Exception("Mysql server has gone away", 2006);
-            }
-            return false;
-        }, E_WARNING);
+        // set_error_handler(function($errno, $errstr, $errfile, $errline, array $errcontext) {
+        //     if (strpos($errstr, 'PDO::__construct(): MySQL server has gone away') !== false || strpos($errstr, "Error while sending QUERY packet") !== false) {
+        //         throw new \Conpoz\Core\Lib\Db\DBQuery\Exception("Mysql server has gone away", 2006);
+        //     }
+        //     return false;
+        // }, E_WARNING);
 
         register_shutdown_function(function(\Conpoz\Core\Lib\Db\DBQuery $obj) {
-            if($obj->success && $obj->db->inTransaction()) {
-                if(!$obj->db->rollBack()) {
+            if($obj->success[SELF::MASTER_RESOURCE_ID] && $obj->db[SELF::MASTER_RESOURCE_ID]->inTransaction()) {
+                if(!$obj->db[SELF::MASTER_RESOURCE_ID]->rollBack()) {
                     $obj->db = null;
                 }
             }
         }, $this);
-
-        if (empty($params['adapter']) || empty($params['dbname']) || empty($params['host']) || empty($params['username']) || empty($params['password'])) {
-            throw new \Conpoz\Core\Lib\Db\DBQuery\Exception("Mysql Db connected miss params");
+        
+        foreach ($dsnSet as $key => $val) {
+            switch ($key) {
+                case 'master':
+                    if (empty($val['adapter']) || empty($val['dbname']) || empty($val['host']) || empty($val['username']) || empty($val['password'])) {
+                        throw new \Conpoz\Core\Lib\Db\DBQuery\Exception('Mysql Db master miss params');
+                    }
+                    $val['charset'] = isset($val['charset']) && !empty($val['charset']) ? $val['charset'] : 'utf8';
+                    $this->dsnSet[0] = $val['adapter'] . ':dbname=' .$val['dbname'] . ';host=' . $val['host'] . ';charset=' . $val['charset'];
+                    $this->username[0] = $val['username'];
+                    $this->password[0] = $val['password'];
+                    break;
+                case 'slave':
+                    foreach ($val as $slaveKey => $slaveVal) {
+                        if (empty($slaveVal['adapter']) || empty($slaveVal['dbname']) || empty($slaveVal['host']) || empty($slaveVal['username']) || empty($slaveVal['password'])) {
+                            throw new \Conpoz\Core\Lib\Db\DBQuery\Exception('Mysql Db slave ' . $slaveKey . ' miss params');
+                        }
+                        $slaveIndex = $slaveKey + 1;
+                        $slaveVal['charset'] = isset($slaveVal['charset']) && !empty($slaveVal['charset']) ? $slaveVal['charset'] : 'utf8';
+                        $this->dsnSet[$slaveIndex] = $slaveVal['adapter'] . ':dbname=' .$slaveVal['dbname'] . ';host=' . $slaveVal['host'] . ';charset=' . $slaveVal['charset'];
+                        $this->username[$slaveIndex] = $slaveVal['username'];
+                        $this->password[$slaveIndex] = $slaveVal['password'];
+                    }
+                    break;
+                default:
+                    throw new \Conpoz\Core\Lib\Db\DBQuery\Exception("Mysql Db config format error");
+            }
         }
-
-        $params['charset'] = isset($params['charset']) && !empty($params['charset']) ? $params['charset'] : 'utf8';
-        $this->dsn = $params['adapter'] . ':dbname=' .$params['dbname'] . ';host=' . $params['host'] . ';charset=' . $params['charset'];
-        $this->username = $params['username'];
-        $this->password = $params['password'];
-        $this->connect();
     }
 
-    public function connect ()
+    public function connect ($dbEnv = SELF::MASTER_RESOURCE_ID)
     {
         try {
-            $this->db = new \PDO($this->dsn, $this->username, $this->password, array(
-                \PDO::ATTR_PERSISTENT => true
-            ));
-        }
-        catch (\PDOException $e) {
+            switch ($dbEnv) {
+                case SELF::MASTER_RESOURCE_ID:
+                    $this->db[SELF::MASTER_RESOURCE_ID] = new \PDO($this->dsnSet[0], $this->username[0], $this->password[0], array(
+                        \PDO::ATTR_PERSISTENT => true
+                    ));
+                    break;
+                case SELF::SLAVE_RESOURCE_ID:
+                    $slaveIndex = rand(1, count($this->dsnSet) - 1);
+                    $this->db[SELF::SLAVE_RESOURCE_ID] = new \PDO($this->dsnSet[$slaveIndex], $this->username[$slaveIndex], $this->password[$slaveIndex], array(
+                        \PDO::ATTR_PERSISTENT => true
+                    ));
+                    break;
+            }
+        } catch (\PDOException $e) {
             $this->errorInfo = $e->getMessage();
             throw new \Conpoz\Core\Lib\Db\DBQuery\Exception($this->errorInfo);
             return false;
         }
-        $this->success = true;
+        $this->success[$dbEnv] = true;
         return true;
     }
 
@@ -82,10 +113,11 @@ class DBQuery
 
     public function begin() 
     {
-        if (!$this->success) {
-            throw new \Conpoz\Core\Lib\Db\DBQuery\Exception('Mysql db is not connected');
+        $this->focusMaster = true;
+        if (!$this->success[SELF::MASTER_RESOURCE_ID]) {
+            $this->connect(SELF::MASTER_RESOURCE_ID);
         }
-        $this->db->beginTransaction();
+        $this->db[SELF::MASTER_RESOURCE_ID]->beginTransaction();
         return $this;
     }
 
@@ -96,11 +128,11 @@ class DBQuery
 
     public function commit() 
     {
-        if (!$this->success) {
-            throw new \Conpoz\Core\Lib\Db\DBQuery\Exception('Mysql db is not connected');
+        if (!$this->success[SELF::MASTER_RESOURCE_ID]) {
+            $this->connect(SELF::MASTER_RESOURCE_ID);
         }
         $this->beforeCommit();
-        $return = $this->db->commit();
+        $return = $this->db[SELF::MASTER_RESOURCE_ID]->commit();
         $this->afterCommit($return);
         return $return;
     }
@@ -117,11 +149,11 @@ class DBQuery
 
     public function rollback() 
     {
-        if (!$this->success) {
-            throw new \Conpoz\Core\Lib\Db\DBQuery\Exception('Mysql db is not connected');
+        if (!$this->success[SELF::MASTER_RESOURCE_ID]) {
+            $this->connect(SELF::MASTER_RESOURCE_ID);
         }
         $this->beforeRollback();
-        $return = $this->db->rollBack();
+        $return = $this->db[SELF::MASTER_RESOURCE_ID]->rollBack();
         $this->afterRollback($return);
         return $return;
     }
@@ -131,16 +163,39 @@ class DBQuery
         //do nothign
     }
 
-    public function execute($sql, array $params = array()) 
+    public function execute($sql, array $params = array(), $dbEnv = SELF::AUTO_RESOURCE_ID) 
     {
-        $this->sth = null;
-        if (!$this->success) {
-            throw new \Conpoz\Core\Lib\Db\DBQuery\Exception('Mysql db is not connected');
-        } 
         if (empty($sql)) {
             throw new \Conpoz\Core\Lib\Db\DBQuery\Exception('Mysql sql statement is required');
         }
-        $this->sth = $this->db->prepare($sql);  
+        
+        $resourceIndex = null;
+        switch ($dbEnv) {
+            case SELF::MASTER_RESOURCE_ID:
+                $resourceIndex = SELF::MASTER_RESOURCE_ID;
+                $this->focusMaster = true;
+                break;
+            case SELF::SLAVE_RESOURCE_ID:
+                $resourceIndex = SLAVE_RESOURCE_ID;
+                break;
+            default:
+                if ($this->focusMaster) {
+                    $resourceIndex = SELF::MASTER_RESOURCE_ID;
+                } else {
+                    if (stripos(trim($sql), 'SELECT') === 0) {
+                        $resourceIndex = SELF::SLAVE_RESOURCE_ID;
+                    } else {
+                        $resourceIndex = SELF::MASTER_RESOURCE_ID;
+                        $this->focusMaster = true;
+                    }
+                }
+        }
+        
+        $this->sth = null;
+        if (!$this->success[$resourceIndex]) {
+            $this->connect($resourceIndex);
+        } 
+        $this->sth = $this->db[$resourceIndex]->prepare($sql);  
         foreach ($params as $k => $v) {
             $bindType = SELF::$bindType['others'];
             if (isset(SELF::$bindType[gettype($v)])) {
@@ -157,7 +212,7 @@ class DBQuery
         * execute and handle deadlock, and redo for setting times
         **/
         $retry = 0;
-        while ($retry <= $this->deadlockRetryTimes) {
+        while (true) {
             try {
                 $success = $this->sth->execute();
                 /**
@@ -165,6 +220,9 @@ class DBQuery
                 **/
                 break;
             } catch (\PDOException $e) {
+                if ($retry <= $this->deadlockRetryTimes) {
+                    throw $e;
+                }
                 /**
                 * $e->errorInfo[0]==40001 (ISO/ANSI) Serialization failure, e.g. timeout or deadlock;
                 * $e->errorInfo[1]==1213 (MySQL SQLSTATE) ER_LOCK_DEADLOCK
@@ -195,9 +253,6 @@ class DBQuery
 
     public function insert($table, array $data = array()) 
     {
-        if (!$this->success) {
-            throw new \Conpoz\Core\Lib\Db\DBQuery\Exception('Mysql db is not connected');
-        } 
         if (empty($data)) {
             throw new \Conpoz\Core\Lib\Db\DBQuery\Exception('Mysql insert data is required');
         } 
@@ -211,7 +266,7 @@ class DBQuery
         $columnsStr = '(' . $columnsStr . ')';
         $valuesBindStr = '(:' . implode(',:', array_keys($this->data)) . ')';
         $sql = 'INSERT INTO ' . $table .' '. $columnsStr . ' VALUES ' . $valuesBindStr;
-        $rh = $this->execute($sql, $this->data);
+        $rh = $this->execute($sql, $this->data, SELF::MASTER_RESOURCE_ID);
         $rh->lastInsertId = $this->db->lastInsertId();
         $this->afterInsert($rh);
         return $rh;
@@ -229,9 +284,6 @@ class DBQuery
 
     public function update($table, array $data, $conditions = null, array $params = array()) 
     {
-        if (!$this->success) {
-            throw new \Conpoz\Core\Lib\Db\DBQuery\Exception('Mysql db is not connected');
-        } 
         if (empty($data)) {
             throw new \Conpoz\Core\Lib\Db\DBQuery\Exception('Mysql update data is required');
         } 
@@ -256,7 +308,7 @@ class DBQuery
         }
         $updateStr = trim($updateStr, ',');
         $sql = 'UPDATE ' . $table . ' SET ' . $updateStr . ' WHERE ' . $conditions;
-        $rh = $this->execute($sql, array_merge($paramsAry, $params));
+        $rh = $this->execute($sql, array_merge($paramsAry, $params), SELF::MASTER_RESOURCE_ID);
         $this->afterUpdate($rh);
         return $rh;
     }
@@ -273,9 +325,6 @@ class DBQuery
 
     public function delete($table, $conditions = null, array $params = array()) 
     {
-        if (!$this->success) {
-            throw new \Conpoz\Core\Lib\Db\DBQuery\Exception('Mysql db is not connected');
-        }
         if (empty($table)) {
             throw new \Conpoz\Core\Lib\Db\DBQuery\Exception('Mysql db table is required');
         }
@@ -288,7 +337,7 @@ class DBQuery
         }
         $this->beforeDelete();
         $sql = 'DELETE ' . $table . ' FROM ' . $table . ' WHERE ' . $conditions;
-        $rh = $this->execute($sql, $params);
+        $rh = $this->execute($sql, $params, SELF::MASTER_RESOURCE_ID);
         $this->afterDelete($rh);
         return $rh;
     }
