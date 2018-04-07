79a242ce72e4ac9ffc93352a54d5a46dcbed80c0
hikaruocean <kensou0012@gmail.com>
2018-04-07 17:01:19 +0800

    job_queue.sql

---
+++ job_queue.sql
@@ -0,0 +1,29 @@
+-- --------------------------------------------------------
+-- 主機:                           127.0.0.1
+-- 伺服器版本:                        10.0.33-MariaDB-0ubuntu0.16.04.1 - Ubuntu 16.04
+-- 伺服器操作系統:                      debian-linux-gnu
+-- HeidiSQL 版本:                  9.5.0.5196
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- 傾印  表格 tot.job_queue 結構
+CREATE TABLE IF NOT EXISTS `job_queue` (
+  `job_queue_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(16) NOT NULL DEFAULT '',
+  `params` varchar(256) NOT NULL DEFAULT '',
+  `status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '-1: 處理失敗 0: 未處理, 1: 進行中, 2: 處理成功',
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`job_queue_id`),
+  KEY `idx-status-job_queue_id` (`status`,`job_queue_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+
+-- 取消選取資料匯出。
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
